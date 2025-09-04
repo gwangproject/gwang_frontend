fde2baa0c05097abd9c0a75c33b83079cf49b3d4
seungju <lsjxcs@gmail.com>
2025-09-04 20:50:12 +0900

    임시저장

---
+++ gwang-project/src/pages/Login.jsx
@@ -0,0 +1,20 @@
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+export const Login = () => {
+  const REST_API_KEY = "백엔드한테 달라하자1";
+  const REDIRECT_URI = "백엔드한테 달라하자2";
+  const link = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+
+  const loginHandler = () => {
+    window.location.href = link;
+  };
+
+  return (
+    <div>
+      <button type="button" onClick={loginHandler}>
+        로그인 하기
+      </button>
+    </div>
+  );
+};
